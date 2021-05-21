62e9d4a1303905088951605d0fe3353ae98916dd
Irsacfac <irsacfac@gmail.com>
2021-05-20 18:42:09 -0600

    Update ContainerProblem.py
    Constructor ContainerProblem

--- app/ContainerProblem.py
+++ app/ContainerProblem.py
@@ -1,2 +1,5 @@
 class ContainerProblem:
-    def __init__(self):
+    def __init__(self, maxWeight, elementsWeight, elementsProfit):
+        self.maxWeight = maxWeight
+        self.weights = elementsWeight
+        self.profits = elementsProfit
