3270b51e75666c70804b3c2ea57ad0ed6f2d7f48
Irsacfac <irsacfac@gmail.com>
2021-05-25 22:59:35 -0600

    Update ContainerProblem.py

--- app/ContainerProblem.py
+++ app/ContainerProblem.py
@@ -5,6 +5,10 @@ class ContainerProblem:
         self.profits = elementsProfit
         self.loop = loops
 
+        #to track the combination that gave the maximum profit
+        self.maxProfit = 0
+        self.itemList = []
+
     #return a list of the items which combination gets the max possible value of all
     def bruteForce(self):
         if(self.weights[-1] < self.maxWeight):
@@ -41,3 +45,15 @@ class ContainerProblem:
     #return a list of items of the max value that included two specific items
     def bruteForceIteration(self, fixedItem, currentItem):
         return []
+
+    #if it is possible add the next item position to the list
+    def addNextItem(self, actualWeight, actualItem, actualValue, actualList, maxLengtList):
+        if actualWeight == self.maxWeight:
+            return
+        while actualItem < maxLengtList:
+            if actualWeight + self.weights[actualItem] <= self.maxWeight:
+                if(actualValue + self.profits[actualItem] > self.maxProfit):
+                    self.maxProfit = actualValue + self.profits[actualItem]
+                    self.itemList = actualList + [actualItem]
+                self.addNextItem(actualWeight + self.weights[actualItem], actualItem + 1, actualValue + self.profits[actualItem], actualList + [actualItem], maxLengtList)
+            actualItem = actualItem + 1
