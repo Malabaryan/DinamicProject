618cf1c60a73afce5832288ec3e459cccf06c917
Bryan Hernandez O <hernandez.bryan.ho@gmail.com>
2021-05-29 19:13:08 -0600

    Backtracking ready

--- app/GoldProblem.py
+++ app/GoldProblem.py
@@ -1,59 +1,68 @@
 import sys
+import numpy as np
 
-def checkLAbyrint(maxColumna, maxFila, matrix):
+def checkLAbyrint(matrix):
+    maxFila = len(matrix)
+    maxColumna = len(matrix[0])
 
-    return checkNext(maxColumna, maxFila, 0, 0, matrix, 0)
+    for row in matrix:
+        row.reverse()
+
+    valoresFinales = []
+    for rowNumber in range(maxFila):
+        valores = []
+        valoresFinales.append( [checkNext(rowNumber, 0, matrix, matrix[rowNumber][0], valores), valores])
+
+    checkMax = []
+    for row in valoresFinales:
+        checkMax.append(row[0])
+
+    return valoresFinales[checkMax.index(max(checkMax))]
 
 
-def checkNext(maxColumna, maxFila, columna, fila, matrix, valor):
+def checkNext( fila, columna, matrix, valor, valores):
+    maxFila = len(matrix)
+    maxColumna = len(matrix[0])
     valor1 = 0
     valor2 = 0
     valor3 = 0
+    valores.append([fila, columna])
+    print("Valor actual: " + str(valor))
 
-    if (columna < maxColumna):
+    if (columna < maxColumna-1):
 
         # Revisar diagonal arriba
         if (fila > 0):
-            print("valor seleccionado: " + str(matrix[fila][columna]))
-            print("fila seleccionada: " + str(fila))
-            print("columna seleccionada: " + str(columna))
-            print("\n")
-            valor1 = valor + matrix[fila][columna]
-            return checkNext(maxColumna, maxFila, columna + 1, fila - 1, matrix,  valor + matrix[fila][columna]);
+            valor1 = valor + matrix[fila-1][columna+1]
 
         # Revisar diagonal abajo
-        if (fila < maxFila):
-            print("valor seleccionado: " + str(matrix[fila][columna]))
-            print("fila seleccionada: " + str(fila))
-            print("columna seleccionada: " + str(columna))
-            print("\n")
-            valor2 = valor + matrix[fila][columna]
-            return checkNext(maxColumna, maxFila, columna + 1, fila + 1, matrix, valor + matrix[fila][columna]);
+        if (fila < maxFila-1):
+            valor2 = valor + matrix[fila+1][columna+1]
 
         # Revisar derecho
-        print("valor seleccionado: " + str(matrix[fila][columna]))
-        print("fila seleccionada: " + str(fila))
-        print("columna seleccionada: " + str(columna))
-        print("\n")
-        valor3 = valor + matrix[fila][columna]
-        return checkNext(maxColumna, maxFila, columna + 1, fila, matrix, valor + matrix[fila][columna]);
+        valor3 = valor + matrix[fila][columna+1]
 
-        print(str(valor1) + " " + str(valor2) + " " +str(valor3) + " ")
-        if(valor1 >= valor2 and valor1 >= valor3):
-            print("El valor " + str(valor1) + " es el mayor.")
-            return valor1
-        if (valor2 >= valor1 and valor2 >= valor3):
-            print("El valor " + str(valor2) + " es el mayor.")
-            return valor2
-        if (valor3 >= valor2 and valor3 >= valor1):
-            print("El valor " + str(valor3) + " es el mayor.")
-            return valor3
+        listValores = [valor1, valor2, valor3]
+        maximo = max(listValores)
+        print(listValores)
+        print("El valor maximo es: " + str(maximo) + "\n")
+
+        if(valor1 == maximo):
+            #valores.append([fila - 1, columna + 1])
+            return checkNext( fila - 1  , columna + 1, matrix,  valor1, valores)
+        if (valor2 == maximo):
+            #valores.append([fila + 1, columna + 1])
+            return checkNext( fila + 1  , columna + 1, matrix,  valor2, valores)
+        if (valor3 == maximo):
+            #valores.append([fila , columna + 1])
+            return checkNext( fila      , columna + 1, matrix,  valor3, valores)
+
     return valor
 
-matrix = [[1,2,3],
-          [4,5,6],
-          [7,8,9]]
-print(matrix)
-valorF = checkLAbyrint(3,3,matrix)
+matrix = [[1,1,1,9,1,9,9],
+          [1,1,9,1,9,1,1],
+          [9,9,1,1,1,1,1],
+          [1,1,9,10,10,10,10]]
 
-print(valorF)
+print(checkLAbyrint(matrix))
+
